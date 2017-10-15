28c97ec48bb635d56b9c36e094e91822e090b88a
Isaac Ashwin <ashiswin@gmail.com>
2017-10-16 01:18:37 +0800

    Added z computation and updated test results

--- work/verilog/alu_2.v
+++ work/verilog/alu_2.v
@@ -23,6 +23,10 @@ module alu_2 (
   
   reg [7:0] result;
   
+  reg zComp;
+  
+  integer i;
+  
   always @* begin
     zOut = 1'h0;
     vOut = 1'h0;
@@ -35,12 +39,22 @@ module alu_2 (
         out = result;
         nOut = result[7+0-:1];
         vOut = (op1[7+0-:1] & op2[7+0-:1] & ~result[7+0-:1]) | (~op1[7+0-:1] & ~op2[7+0-:1] & result[7+0-:1]);
+        zComp = result[0+0-:1];
+        for (i = 1'h1; i < 4'h8; i = i + 1) begin
+          zComp = ~(zComp | result[(i)*1+0-:1]);
+        end
+        zOut = zComp;
       end
       6'h01: begin
         result = op1 - op2;
         out = result;
         nOut = result[7+0-:1];
         vOut = (op1[7+0-:1] & op2[7+0-:1] & ~result[7+0-:1]) | (~op1[7+0-:1] & ~op2[7+0-:1] & result[7+0-:1]);
+        zComp = result[0+0-:1];
+        for (i = 1'h1; i < 4'h8; i = i + 1) begin
+          zComp = ~(zComp | result[(i)*1+0-:1]);
+        end
+        zOut = zComp;
       end
       6'h02: begin
         out = op1 * op2;
